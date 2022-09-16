ff27734d91a19d60555300b86f833938380beb9e
johanneksen <johannes.marn@protonmail.com>
2022-09-16 16:58:25 +0200

    added CI colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			purple: '#591647',
+			rose: '#d8a588',
+			amber: '#a54521',
+			yellow: '#fecb13',
+		}
 	},
 
 	plugins: []
