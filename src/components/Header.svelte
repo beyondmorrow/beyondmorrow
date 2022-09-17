b067e9fe21b1e1456aee45c66d827c6d9e548b4e
johanneksen <johannes.marn@protonmail.com>
2022-09-17 10:54:48 +0200

    improved mobile nav

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,37 @@
 <script>
-	let displayMobileNav = false;
+	import { fade, scale } from 'svelte/transition';
 
+	let displayMobileNav = false;
 	function handleMobileNav() {
 		displayMobileNav = !displayMobileNav;
 	}
+
+	const navEntries = [
+		{
+			name: 'Über mich',
+			link: '/about'
+		},
+		{
+			name: 'Yoga',
+			link: '/yoga'
+		},
+		{
+			name: 'Meditation',
+			link: '/meditation'
+		},
+		{
+			name: 'Healing Arts',
+			link: '/healing-arts'
+		},
+		{
+			name: 'Retreats & Events',
+			link: '/retreats-events'
+		},
+		{
+			name: 'Kontakt',
+			link: '/contact'
+		}
+	];
 </script>
 
 <section>
@@ -11,33 +39,20 @@
 		<div class="container px-4 mx-auto">
 			<div class="flex items-center justify-between relative">
 				<a class="inline-block" href="/">
-					<img src="pstls-assets/logos/pstls-logo-md.svg" alt="" width="116" />
+					<img src="/logo/logo-text.png" alt="logo" class="w-40 sm:w-72"/>
 				</a>
 				<button on:click={handleMobileNav} class="lg:hidden navbar-burger">
-					<svg
-						width="24"
-						height="24"
-						viewbox="0 0 24 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fill-rule="evenodd"
-							clip-rule="evenodd"
-							d="M3.5 7C3.22386 7 3 6.77614 3 6.5C3 6.22386 3.22386 6 3.5 6H20.5C20.7761 6 21 6.22386 21 6.5C21 6.77614 20.7761 7 20.5 7H3.5ZM3.5 12C3.22386 12 3 11.7761 3 11.5C3 11.2239 3.22386 11 3.5 11H20.5C20.7761 11 21 11.2239 21 11.5C21 11.7761 20.7761 12 20.5 12H3.5ZM3 16.5C3 16.7761 3.22386 17 3.5 17H20.5C20.7761 17 21 16.7761 21 16.5C21 16.2239 20.7761 16 20.5 16H3.5C3.22386 16 3 16.2239 3 16.5Z"
-							fill="#000"
-						/>
-					</svg>
+					<img src="/icons/nav-burger.svg" alt="Menü" class="w-7"/>
 				</button>
 				<div class="hidden lg:block ml-auto mr-14">
 					<ul class="inline-flex">
-						<li class="mr-8"><a class="inline-block font-bold hover:text-gray-900" aria-current="page" href="/">Home</a></li>
-						<li class="mr-8"><a class="inline-block font-bold hover:text-gray-900" href="/about">Über mich</a></li>
-						<li class="mr-8"><a class="inline-block font-bold hover:text-gray-900" href="/yoga">Yoga</a></li>
-						<li class="mr-8"><a class="inline-block font-bold hover:text-gray-900" href="/meditation">Meditation</a></li>
-                        <li class="mr-8"><a class="inline-block font-bold hover:text-gray-900" href="/healing-arts">Healing Arts</a></li>
-                        <li class="mr-8"><a class="inline-block font-bold hover:text-gray-900" href="/retreats-events">Reatreats & Events</a></li>
-                        <li class="mr-8"><a class="inline-block font-bold hover:text-gray-900" href="/contact">Kontakt</a></li>
+						{#each navEntries as entry}
+							<li class="mr-8">
+								<a class="inline-block font-bold hover:text-gray-900" href={entry.link}
+									>{entry.name}</a
+								>
+							</li>
+						{/each}
 					</ul>
 				</div>
 				<div class="hidden lg:block">
@@ -49,39 +64,35 @@
 		</div>
 	</nav>
 
+	<!-- MOBILE NAV -->
 	{#if displayMobileNav}
-		<div class="navbar-menu fixed top-0 left-0 bottom-0 w-3/4 max-w-xs z-50" data-config-="">
-			<div class="navbar-backdrop fixed inset-0 bg-black bg-opacity-75" />
-			<nav class="relative flex flex-col h-full p-8 bg-white">
-				<div class="flex items-center justify-between mb-12">
-					<a class="items-center" href="/">
-						<img src="pstls-assets/logos/pstls-logo-md.svg" alt="" width="102" />
-					</a>
-					<button on:click={handleMobileNav}
-						class="inline-block navbar-close focus:outline-none"
-						type="button"
-						aria-label="Close"
-					>
-						CLOSE
-					</button>
-				</div>
-				<div>
-					<ul>
-                        <li on:click={handleMobileNav} class="py-3"><a class="inline-block font-bold hover:text-gray-900" aria-current="page" href="/">Home</a></li>
-						<li on:click={handleMobileNav} class="py-3"><a class="inline-block font-bold hover:text-gray-900" href="/about">Über mich</a></li>
-						<li on:click={handleMobileNav} class="py-3"><a class="inline-block font-bold hover:text-gray-900" href="/yoga">Yoga</a></li>
-						<li on:click={handleMobileNav} class="py-3"><a class="inline-block font-bold hover:text-gray-900" href="/meditation">Meditation</a></li>
-                        <li on:click={handleMobileNav} class="py-3"><a class="inline-block font-bold hover:text-gray-900" href="/healing-arts">Healing Arts</a></li>
-                        <li on:click={handleMobileNav} class="py-3"><a class="inline-block font-bold hover:text-gray-900" href="/retreats-events">Reatreats & Events</a></li>
-                        <li on:click={handleMobileNav} class="py-3"><a class="inline-block font-bold hover:text-gray-900" href="/contact">Kontakt</a></li>
-					</ul>
-				</div>
-				<div class="mt-auto">
-					<div>
-						<a class="block py-3 text-center font-bold" href="/">Log In</a>
+			<div in:scale out:fade class="fixed top-0 left-0 bottom-0 w-full z-50" data-config-="">
+				<nav class="relative flex flex-col h-full p-8 bg-white">
+					<div class="flex items-center justify-between mb-12">
+						<a on:click={handleMobileNav} class="items-center" href="/">
+							<img src="/logo/logo-image.png" alt="logo" class="w-14"/>
+						</a>
+						<button
+							on:click={handleMobileNav}
+							class="inline-block focus:outline-none"
+							type="button"
+							aria-label="Close"
+						>
+							<img src="/icons/close-button.svg" alt="X" class="w-7"/>
+						</button>
 					</div>
-				</div>
-			</nav>
-		</div>
+					<div class="text-center">
+						<ul>
+							{#each navEntries as entry}
+								<li on:click={handleMobileNav} class="py-3">
+									<a class="inline-block font-bold hover:text-gray-900" href={entry.link}
+										>{entry.name}</a
+									>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				</nav>
+			</div>
 	{/if}
 </section>
