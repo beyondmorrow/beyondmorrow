601d7f33974b3128b5a478a6221b87d6411c4cc4
johanneksen <johannes.marn@protonmail.com>
2022-09-18 10:45:24 +0200

    render every second detailed offer differently

--- src/components/DetailedOffers.svelte
+++ src/components/DetailedOffers.svelte
@@ -3,6 +3,12 @@
 	import Button from './Button.svelte';
 
 	export let blok;
+
+	let offerCount = 0;
+	function countAndChangeTemplate() {
+		offerCount++;
+		return offerCount % 2 === 0;
+	}
 </script>
 
 <div use:storyblokEditable={blok}>
@@ -10,35 +16,67 @@
 		<section id={offer.anchor} class="py-24">
 			<div class="container px-4 mx-auto max-w-6xl">
 				<div class="flex flex-wrap items-center">
-					<div class="w-full lg:w-2/3 px-4 mb-16 lg:mb-0">
-						<div class="text-left">
-							<h2
-								class="font-spectral text-4xl lg:text-5xl font-heading mb-9 text-beyondpurple-900"
-							>
-								{offer.heading}
-							</h2>
-							<p class="leading-8 mb-14">
-								{offer.text}
-							</p>
-							<h3 class="font-spectral italic text-2xl">
-								{offer.secondHeading}
-							</h3>
-							<ul class="leading-8 list-disc pl-6">
-								{#each offer.bulletPoints as bulletPoint}
-									<li>{bulletPoint.text}</li>
-								{/each}
-							</ul>
+					{#if countAndChangeTemplate()}
+						<div class="w-full lg:w-2/3 px-4 mb-16 lg:mb-0">
+							<div class="text-left">
+								<h2
+									class="font-spectral text-4xl lg:text-5xl font-heading mb-9 text-beyondpurple-900"
+								>
+									{offer.heading}
+								</h2>
+								<p class="leading-8 mb-14">
+									{offer.text}
+								</p>
+								<h3 class="font-spectral italic text-2xl">
+									{offer.secondHeading}
+								</h3>
+								<ul class="leading-8 list-disc pl-6">
+									{#each offer.bulletPoints as bulletPoint}
+										<li>{bulletPoint.text}</li>
+									{/each}
+								</ul>
+							</div>
 						</div>
-					</div>
-					<div class="w-full lg:w-1/3 px-4">
-						<div class="relative">
-							<img
-								class="block w-full relative"
-								src="https://a.storyblok.com/f/174226/1365x2048/ddeb605479/kakaolove-8.jpg"
-								alt=""
-							/>
+						<div class="w-full lg:w-1/3 px-4">
+							<div class="relative">
+								<img
+									class="block w-full relative"
+									src={offer.image.filename}
+									alt={offer.image.alt}
+								/>
+							</div>
 						</div>
-					</div>
+					{:else}
+						<div class="w-full lg:w-1/3 px-4">
+							<div class="relative">
+								<img
+									class="block w-full relative"
+									src={offer.image.filename}
+									alt={offer.image.alt}
+								/>
+							</div>
+						</div>
+						<div class="w-full lg:w-2/3 px-4 mb-16 lg:mb-0">
+							<div class="text-left">
+								<h2
+									class="font-spectral text-4xl lg:text-5xl font-heading mb-9 text-beyondpurple-900"
+								>
+									{offer.heading}
+								</h2>
+								<p class="leading-8 mb-14">
+									{offer.text}
+								</p>
+								<h3 class="font-spectral italic text-2xl">
+									{offer.secondHeading}
+								</h3>
+								<ul class="leading-8 list-disc pl-6">
+									{#each offer.bulletPoints as bulletPoint}
+										<li>{bulletPoint.text}</li>
+									{/each}
+								</ul>
+							</div>
+						</div>
+					{/if}
 				</div>
 				<div class="mt-5 text-center">
 					<Button link="/{offer.callToActionLink.cached_url}" text={offer.callToAction} />
