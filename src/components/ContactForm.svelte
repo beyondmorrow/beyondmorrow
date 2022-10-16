a6e11139c33853526a063d128657d13ca00e3c4b
johanneksen <johannes.marn@protonmail.com>
2022-10-16 16:50:53 +0200

    next one...

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -88,6 +88,7 @@
 								rows="10"
 								placeholder="Deine Nachricht"
 							/>
+              <input type="hidden" name="contact" value="contact">
 							<button
 								type="submit"
 								class="rounded-sm drop-shadow-md inline-block w-full md:w-auto py-3 px-8 text-center text-white font-bold bg-beyondpurple-900 hover:bg-beyondpurple-800 transition duration-200"
