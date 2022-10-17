a31a77bee4a9f1e25cfd9c881e7e489c9bcea89b
johanneksen <johannes.marn@protonmail.com>
2022-10-17 20:35:59 +0200

    try busboy for parsing multipart form data

--- functions/contactForm.js
+++ functions/contactForm.js
@@ -1,3 +1,4 @@
+import * as Busboy from "busboy"
 const client = require('@sendgrid/mail');
 const {
     SENDGRID_API_KEY,
@@ -6,18 +7,18 @@ const {
 } = process.env;
 
 exports.handler = async (event, context) => {
-    const { name, surname, email, message } = JSON.parse(event.body);
+    const fields = await parseMultipartForm(event);
     client.setApiKey(SENDGRID_API_KEY);
 
     const data = {
         to: SENDGRID_TO_EMAIL,
         from: SENDGRID_FROM_EMAIL,
-        subject: `Neue Kontaktanfrage von ${name} ${surname} (${email})`,
-        html: message,
+        subject: `Neue Kontaktanfrage von ${fields.name} ${fields.surname} (${fields.email})`,
+        html: fields.message,
     };
 
     try {
-        await client.send(data);
+        await client.send(data);    
         return {
             statusCode: 200,
             body: JSON.stringify({ msg: "Message sent"}),
@@ -29,3 +30,48 @@ exports.handler = async (event, context) => {
         };
     }
 }
+
+function parseMultipartForm(event) {
+    return new Promise((resolve) => {
+      // we'll store all form fields inside of this
+      const fields = {};
+  
+      // let's instantiate our busboy instance!
+      const busboy = new Busboy({
+        // it uses request headers
+        // to extract the form boundary value (the ----WebKitFormBoundary thing)
+        headers: event.headers
+      });
+  
+      // before parsing anything, we need to set up some handlers.
+      // whenever busboy comes across a file ...
+      busboy.on(
+        "file",
+        (fieldname, filestream, filename, transferEncoding, mimeType) => {
+          // ... we take a look at the file's data ...
+          filestream.on("data", (data) => {
+            // ... and write the file's name, type and content into `fields`.
+            fields[fieldname] = {
+              filename,
+              type: mimeType,
+              content: data,
+            };
+          });
+        }
+      );
+  
+      // whenever busboy comes across a normal field ...
+      busboy.on("field", (fieldName, value) => {
+        // ... we write its value into `fields`.
+        fields[fieldName] = value;
+      });
+  
+      // once busboy is finished, we resolve the promise with the resulted fields.
+      busboy.on("finish", () => {
+        resolve(fields)
+      });
+  
+      // now that all handlers are set up, we can finally start processing our request!
+      busboy.write(event.body);
+    });
+  }
