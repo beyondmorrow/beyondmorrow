50beea7825416ff511eea6345f67952675b3d3ad
johanneksen <johannes.marn@protonmail.com>
2022-10-26 21:23:45 +0200

    first protype of event section

--- src/components/Events.svelte
+++ src/components/Events.svelte
@@ -1,21 +1,54 @@
 <script>
 	import { useStoryblokApi } from '@storyblok/svelte';
+	import * as c from '../pathConst';
 
-	let story = null;
+	// let events = null;
 
-	async function loadEvents() {
-		const storyblokApi = useStoryblokApi();
-		const data = await storyblokApi.get('cdn/stories/agb', {
-			version: 'published',
-		})
-		story = data.data.story;
-	}
+	// async function loadEvents() {
+	// 	const storyblokApi = useStoryblokApi();
+	// 	const { data } = await storyblokApi.get('cdn/stories', {
+	// 		version: 'published',
+	// 		starts_with: c.PATH_RETREATS_EVENTS.substring(1),
+	// 		excluding_ids: '189787416' // Exclude Retreats & Events Overview Page
+	// 	})
 
-	loadEvents();
+	// 	events = data.stories;
+	// }
 
-	
+	// loadEvents();
+
+	let events = null;
+
+	const storyblokApi = useStoryblokApi();
+	const promise = storyblokApi.get('cdn/stories/', {
+		version: 'published',
+		starts_with: c.PATH_RETREATS_EVENTS.substring(1),
+		excluding_ids: '189787416' // Exclude Retreats & Events Overview Page
+	});
 </script>
 
 <div>
-	{JSON.stringify(story)}
+	{#await promise}
+		<p>Loading ...</p>
+	{:then response}
+		{((events = response.data.stories), '')}
+
+		<div class="bg-blue-400 grid grid-cols-4 gap-5">
+			{#each events as event}
+				<div>
+					<h3>{event.content.body[0].title}</h3>
+					<h4>{event.content.body[0].subtitle}</h4>
+					<p>{event.content.body[0].startingDate}</p>
+
+					{#if event.content.body[0].hasOwnProperty('endDate')}
+						<p>{event.content.body[0].endDate}</p>
+					{/if}
+					<p>{event.content.body[0].type[0]}</p>
+					<p>{event.content.body[0].location}</p>
+				</div>
+			{/each}
+		</div>
+	{:catch error}
+		<p>Leider konnten keine Events geladen werden.</p>
+	{/await}
 </div>
