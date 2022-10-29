78d3097cc38a2b8e7546a87cd3e1ff68ff9dd640
johanneksen <johannes.marn@protonmail.com>
2022-10-29 19:53:17 +0200

    finished event page

--- src/components/nestable/Event.svelte
+++ src/components/nestable/Event.svelte
@@ -12,11 +12,11 @@
 
 <div>
 	<Heading {blok} />
-	<div class="grid grid-cols-2 gap-10">
+	<div class="grid lg:grid-cols-2 gap-10 items-center">
 		<div>
 			<img src={blok.image.filename} alt={blok.image.alt} />
 		</div>
-		<div class="m-10">
+		<div class="mx-10 my-20">
 			<div class="flex items-center justify-center text-3xl mb-5">
 				<img class="h-8" src="/icons/location.svg" alt="location" />
 				<span>{blok.location}</span>
@@ -24,9 +24,17 @@
 			<div class="text-center text-lg">
 				<span>{getFormattedDate(blok, true)}</span>
 			</div>
+			<div class="flex items-center my-5">
+				<div class="flex-grow h-px bg-slate-300" />
+				<span class="flex-shrink text-2xl text-beyondgrey px-3 pb-1">{blok.type[0]}</span>
+				<div class="flex-grow h-px bg-slate-300" />
+			</div>
+			<div class="text-center">
+				<span>{blok.shortDescription}</span>
+			</div>
 		</div>
 	</div>
-	<div class="text-center mt-10">
+	<div class="mt-8">
 		<RichText blok={richText} />
 	</div>
 </div>
