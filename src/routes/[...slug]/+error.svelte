d8713693fa95b3930b91901fb5072fc5d067fa98
johanneksen <johannes.marn@protonmail.com>
2022-11-01 17:03:38 +0100

    added 404 page

--- src/routes/[...slug]/+error.svelte
+++ src/routes/[...slug]/+error.svelte
@@ -1,3 +1,21 @@
-<div>
-    404
+<script>
+	import Heading from "../../components/nestable/Heading.svelte";
+    import * as c from "../../pathConst";
+
+    const heading = {
+        heading: '404',
+        subheading: 'Leider kann die gewünschte Seite nicht geladen werden.'
+    }
+</script>
+
+<div class="container mx-auto text-center mb-24">
+    <Heading blok={heading}  />
+    <div>
+        <ul class="text-lg">
+            <li><a class="underline mb-2" href="{c.PATH_FAQ}">FAQ</a></li>
+            <li><a class="underline mb-2" href="{c.PATH_TIMETABLE}">Stundenplan</a></li>
+            <li><a class="underline mb-2" href="{c.PATH_PRICES}">Preise</a></li>
+            <li><a class="underline mb-2" href="{c.PATH_CONTACT}">Kontakt</a></li>
+        </ul>
+    </div>
 </div>
