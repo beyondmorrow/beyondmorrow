6ae0936eaa6a503fac6e258e00ac9abfa76236ef
johanneksen <johannes.marn@protonmail.com>
2022-11-05 21:49:04 +0100

    modify storyblok button in order it works both internal and for storyblok

--- src/components/nestable/Button.svelte
+++ src/components/nestable/Button.svelte
@@ -7,13 +7,26 @@
 	export let text = 'Buchen';
 	export let target = '_self';
 
+	let storyblokButton = false;
+
 	if (blok) {
 		link = '/' + blok.link;
 		text = blok.text;
+		storyblokButton = true;
 	}
 </script>
 
-<div class="text-center">
+{#if storyblokButton}
+	<div class="text-center">
+		<button>
+			<a
+				class="rounded-sm drop-shadow-md inline-block w-full md:w-auto py-3 px-8 text-center text-white font-bold bg-beyondpurple-900 hover:bg-beyondpurple-800 transition duration-200"
+				{target}
+				href={link}>{text}</a
+			>
+		</button>
+	</div>
+{:else}
 	<button>
 		<a
 			class="rounded-sm drop-shadow-md inline-block w-full md:w-auto py-3 px-8 text-center text-white font-bold bg-beyondpurple-900 hover:bg-beyondpurple-800 transition duration-200"
@@ -21,4 +34,4 @@
 			href={link}>{text}</a
 		>
 	</button>
-</div>
+{/if}
