92d389e4133246053814aaba83ceb2445f374f9e
johanneksen <johannes.marn@protonmail.com>
2022-11-05 21:39:37 +0100

    open storyblok link always with target _self

--- src/components/nestable/Button.svelte
+++ src/components/nestable/Button.svelte
@@ -1,22 +1,24 @@
 <script>
-    import * as c from '../../pathConst';
+	import * as c from '../../pathConst';
 
-    export let blok;
+	export let blok;
 
-    export let link = c.PATH_TIMETABLE;
-    export let text = "Buchen";
-    export let target = "_self";
+	export let link = c.PATH_TIMETABLE;
+	export let text = 'Buchen';
+	export let target = '_self';
 
-    if (blok) {
-        link = '/' + blok.link;
-        text = blok.text;
-    }
+	if (blok) {
+		link = '/' + blok.link;
+		text = blok.text;
+	}
 </script>
 
-<button>
-    <a
-		class="rounded-sm drop-shadow-md inline-block w-full md:w-auto py-3 px-8 text-center text-white font-bold bg-beyondpurple-900 hover:bg-beyondpurple-800 transition duration-200"
-        target={target}
-		href="{link}">{text}</a
-	>
-</button>
+<div class="mx-auto">
+	<button>
+		<a
+			class="rounded-sm drop-shadow-md inline-block w-full md:w-auto py-3 px-8 text-center text-white font-bold bg-beyondpurple-900 hover:bg-beyondpurple-800 transition duration-200"
+			{target}
+			href={link}>{text}</a
+		>
+	</button>
+</div>
