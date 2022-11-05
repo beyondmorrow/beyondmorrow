b5369ad1f5e7e869fe4ceb7e4ef8056a7b74cb53
johanneksen <johannes.marn@protonmail.com>
2022-11-05 21:43:20 +0100

    button text center

--- src/components/nestable/Button.svelte
+++ src/components/nestable/Button.svelte
@@ -13,7 +13,7 @@
 	}
 </script>
 
-<div class="mx-auto">
+<div class="text-center">
 	<button>
 		<a
 			class="rounded-sm drop-shadow-md inline-block w-full md:w-auto py-3 px-8 text-center text-white font-bold bg-beyondpurple-900 hover:bg-beyondpurple-800 transition duration-200"
