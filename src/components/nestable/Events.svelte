8d3a9bce8660b7e20ef4c286a42a82f3eda0d030
johanneksen <johannes.marn@protonmail.com>
2022-11-06 15:35:26 +0100

    event card is link

--- src/components/nestable/Events.svelte
+++ src/components/nestable/Events.svelte
@@ -45,38 +45,38 @@
 						class:pointer-events-none={isPastEvent(event.content.body[0])}
 						class:opacity-30={isPastEvent(event.content.body[0])}
 					>
-						<div class="w-full h-48 relative overflow-hidden">
-							<a href="/{event.full_slug}">
+						<a href="/{event.full_slug}">
+							<div class="w-full h-48 relative overflow-hidden">
 								<img
 									class="absolute block top-0 left-0"
 									src={event.content.body[0].image.filename}
 									alt={event.content.body[0].image.alt}
 								/>
-							</a>
-						</div>
-						<div class="p-5 text-center">
-							<div class="min-h-[80px]">
-								<h3 class="font-spectral text-xl font-semibold italic text-beyondpurple-900">
-									{event.content.body[0].heading}
-								</h3>
-								<h4 class="font-spectral text-md">{event.content.body[0].subheading}</h4>
 							</div>
-							<div class="flex items-center my-3">
-								<div class="flex-grow h-px bg-slate-300" />
-								<span class="flex-shrink text-2xl text-beyondgrey px-3 pb-1"
-									>{event.content.body[0].type[0]}</span
-								>
-								<div class="flex-grow h-px bg-slate-300" />
+							<div class="p-5 text-center">
+								<div class="min-h-[80px]">
+									<h3 class="font-spectral text-xl font-semibold italic text-beyondpurple-900">
+										{event.content.body[0].heading}
+									</h3>
+									<h4 class="font-spectral text-md">{event.content.body[0].subheading}</h4>
+								</div>
+								<div class="flex items-center my-3">
+									<div class="flex-grow h-px bg-slate-300" />
+									<span class="flex-shrink text-2xl text-beyondgrey px-3 pb-1"
+										>{event.content.body[0].type[0]}</span
+									>
+									<div class="flex-grow h-px bg-slate-300" />
+								</div>
+								<div>
+									<span>{getFormattedDate(event.content.body[0])}</span>
+								</div>
+								<div class="flex items-center justify-center">
+									<img class="h-4" src="/icons/location.svg" alt="location" />
+									<span>{event.content.body[0].location}</span>
+								</div>
+								<div><a class="underline" href="/{event.full_slug}">Zum Event</a></div>
 							</div>
-							<div>
-								<span>{getFormattedDate(event.content.body[0])}</span>
-							</div>
-							<div class="flex items-center justify-center">
-								<img class="h-4" src="/icons/location.svg" alt="location" />
-								<span>{event.content.body[0].location}</span>
-							</div>
-							<div><a class="underline" href="/{event.full_slug}">Zum Event</a></div>
-						</div>
+						</a>
 					</div>
 				{/each}
 			</div>
