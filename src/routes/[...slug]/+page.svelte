be5d758ad2bf9c9479cf9c4e6313cb4cebdfcd20
johanneksen <johannes.marn@protonmail.com>
2023-04-24 17:17:01 +0200

    google analyitcs preparation

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { useStoryblokBridge, StoryblokComponent } from '@storyblok/svelte';
+	import { consent } from '../../modules/cookieBanner';
 
 	export let data;
 
@@ -11,7 +12,11 @@
 
 <svelte:head>
 	<title>beyondmorrow - {data.story.name}</title>
-	<meta name="description" content="{data.story.metaDescription}">
+	<meta name="description" content={data.story.metaDescription} />
+
+	{#if $consent === String(true)}
+		<!-- TODO ga script-->>
+	{/if}
 </svelte:head>
 
 <div class="mb-20">
