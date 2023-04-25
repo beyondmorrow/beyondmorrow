a4c3db3ff9f839d7b6cdc2cef543a6e2ea618396
johanneksen <johannes.marn@protonmail.com>
2023-04-25 12:36:38 +0200

    load data client side as a workaround

--- src/routes/[...slug]/+page.js
+++ src/routes/[...slug]/+page.js
@@ -1,3 +1,8 @@
 import { initStoryblok } from "../../storyblok";
 
 initStoryblok();
+
+// Load Data -> TODO remove, it's just a workaround
+export async function load({ params }) {
+    return loadData(params)
+  }
--- src/routes/retreats-events/[...slug]/+page.js
+++
@@ -1,9 +0,0 @@
-import { initStoryblok } from "../../storyblok";
-import loadData from "../../../storyblok";
-
-initStoryblok();
-
-// Load Data
-export async function load({ params }) {
-    return loadData(params)
-  }
