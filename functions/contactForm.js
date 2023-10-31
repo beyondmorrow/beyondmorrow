b60a6605147055ef5f10daf6081c77b1af768ce9
Johannes Marn <johannes.marn@wirecube.at>
2023-10-31 09:53:57 +0100

    switched to mailchimp

--- functions/contactForm.js
+++ functions/contactForm.js
@@ -1,37 +1,50 @@
-import MailService from '@sendgrid/mail'
+import { MailtrapClient } from "mailtrap"
 
 const {
-    SENDGRID_API_KEY,
-    SENDGRID_TO_EMAIL,
-    SENDGRID_FROM_EMAIL
+    MAILTRAP_TOKEN,
+    MAILTRAP_SENDER_EMAIL,
+    MAILTRAP_RECIPIENT_EMAIL
 } = process.env;
 
-exports.handler = async (event, context) => {
+const client = new MailtrapClient({ token: MAILTRAP_TOKEN })
+
+export const handler = async (event) => {
+    const {
+        name,
+        surname,
+        email,
+        message,
+        source
+    } = JSON.parse(event.body);
+
+    let displaySource = source !== '' ? ` - ${source}` : '';
+
+    console.debug(`Try to send message with name: ${name}, surname: ${surname}, email: ${email}, message: ${message}, source: ${source}`)
+
     try {
-        const { name, surname, email, message, source } = JSON.parse(event.body);
-
-        let displaySource = source !== '' ? ` - ${source}` : '';
-
-        MailService.setApiKey(SENDGRID_API_KEY);
-
-        const data = {
-            to: SENDGRID_TO_EMAIL,
-            from: SENDGRID_FROM_EMAIL,
-            reply_to: email,
+        await client.send({
+            from: { name: `${name} ${surname}`, email: MAILTRAP_SENDER_EMAIL },
+            to: [{ email: MAILTRAP_RECIPIENT_EMAIL }],
             subject: `Neue Kontaktanfrage von ${name} ${surname}${displaySource}`,
-            html: message,
-        };
+            text: message,
+            headers: {
+                "Reply-To": email.trim()
+            }
+        })
 
-        await MailService.send(data);
-        
+        console.info("Sent contact form message successfully")
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ msg: "Message sent successfully" }),
-        };
-    } catch (err) {
+            body: JSON.stringify({
+                msg: "Message sent successfully"
+            })
+        }
+    } catch (error) {
+        console.error(`email couldn't be sent: ${error}`)
         return {
-            statusCode: 400,
-            body: JSON.stringify({ msg: err.message }),
-        };
+            statusCode: 500,
+            body: JSON.stringify({ msg: "Error sending the message" })
+        }
     }
 }
